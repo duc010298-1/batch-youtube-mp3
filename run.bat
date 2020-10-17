adf57afb6615893d630f3c4dcdfd99ff9bd2af7b
duc010298 <duc010298@gmail.com>
2020-10-17 17:25:00 +0700

    update

--- run.bat
+++ run.bat
@@ -7,7 +7,7 @@ echo -------Youtube Mp3 Downloader-------
 echo ---------Author: duc010298----------
 echo ------------------------------------
 set /p url="Enter youtube url: "
-set command=youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 --add-metadata --metadata-from-title "%%(artist)s - %%(title)s" --output "%USERPROFILE%\Download\%%(title)s.%%(ext)s" "%url%"
+set command=youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 --add-metadata --metadata-from-title "%%(artist)s - %%(title)s" --output "%USERPROFILE%\Downloads\%%(title)s.%%(ext)s" "%url%"
 echo %command%
-start cmd /k %command%
+start cmd /c %command%
 goto start
